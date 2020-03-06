b165c45e5097ad8b92e04ba30915f3cf4930401c
Kyle Horn <kghorn@gmail.com>
2020-03-06 01:49:43 -0500

    Add data validation functions
    Now instead of simlpe asserts, we have nice data validation functions that print out an error message, the scrape file number, and the book title on unexpected data formatting.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import re
 import bs4
 import csv
 import requests
+from tqdm import tqdm
 
 def _textIsFloat(text):
     '''helper function to check if a piece of text can be converted to a float'''
@@ -13,6 +14,11 @@ def _textIsFloat(text):
     except:
         return False
 
+def dataValidationAssert(assertBool,errorMessage,scrapeNumber,bookData):
+    if not assertBool:
+        bookTitle = bookData['Title'] if 'Title' in bookData else ''
+        raise Exception(errorMessage + f' [scrapeNumber={scrapeNumber}][bookTitle={bookTitle}]')
+
 for scrapeNumber in range(1,50+1):
 
     if os.path.exists(f"./data/Kyle_Horn_books_{scrapeNumber}_of_50.csv"):
@@ -21,134 +27,138 @@ for scrapeNumber in range(1,50+1):
     # get the site to scrape book info from
     url = "http://books.toscrape.com/catalogue/page-{}.html"
     response = requests.get(url.format(scrapeNumber))
-    assert(response.status_code==200) # ensure the page correctly loaded
+    dataValidationAssert(response.status_code==200,f"ERROR: Page did not load correctly (status code {response.status_code})",scrapeNumber,{})
 
     # find the HTML containers holding book information
     soup = bs4.BeautifulSoup(response.text,features='html.parser')
     bookContainers = soup.find_all('article')
-    assert(len(bookContainers)==20) # enforce the 20 book max rule
+    dataValidationAssert(len(bookContainers)<=20,f"ERROR: Too many books scraped (scraped {len(bookContainers)})",scrapeNumber,{})
+    dataValidationAssert(len(bookContainers)>=20,f"ERROR: Too few books scraped (scraped {len(bookContainers)})",scrapeNumber,{})
 
     # for every book we find on the main page, scrape data
     bookDataList = []
-    for book in bookContainers:
+    for book in tqdm(bookContainers):
         bookData = {}
 
         bookData['Main Page Raw HTML'] = soup.text
 
         # extract book title
         titles = book.find_all('a',title=True) #Note: this is the same call as in the "extract book links" code block below. For a more computationally intensive scrape, I wouldn't do this, but in this case I'd prefer for each piece of data to be isolated to make testing cleaner.
-        assert(len(titles)>0) # we must find at least one link per book
+        dataValidationAssert(len(titles)>0, "ERROR: No title found",scrapeNumber,bookData)
         title = titles[0]['title'] #Note: for a more robust scraper, we should check to make sure all of the links match up, instead of simply taking the first
         bookData['Title'] = title
 
         # extract book links
         links = book.find_all('a',href=True)
-        assert(len(links)>0) # we must find at least one link per book
+        dataValidationAssert(len(links)>0,"ERROR: No book sub-link found",scrapeNumber,bookData)
         link = links[0]['href'] #Note: for a more robust scraper, we should check to make sure all of the links match up, instead of simply taking the first
-        assert(link.split('.')[-1] in ['html','htm']) # ensure that our scraped link is truly a website
+        dataValidationAssert(link.split('.')[-1] in ['html','htm'],"ERROR: Book sub-page link lacks an HTML extension",scrapeNumber,bookData)
         bookData['Sub-Page Link'] = link
 
         # extract book ratings
         starRatingScores = {'one':1,'two':2,'three':3,'four':4,'five':5}
         starRatings = book.select('p[class*="star-rating"]')
-        assert(len(starRatings)==1) # there should be only one rating per book
+        dataValidationAssert(len(starRatings)==1, "ERROR: Star rating missing or duplicated",scrapeNumber,bookData)
         starRating = starRatings[0]['class']
-        assert(len(starRating)==2) # this should be a list; the first element is 'star-rating', the second is the rating itself
+        dataValidationAssert(len(starRating)==2,"ERROR: Malformed star rating",scrapeNumber,bookData) # this should be a list; the first element is 'star-rating', the second is the rating itself
         starRatingText = starRating[1].lower().strip()
-        assert(starRatingText in starRatingScores) # we must recognize the rating, so we can cast it from a string to a number
+        dataValidationAssert(starRatingText in starRatingScores, "ERROR: Star rating value not readable",scrapeNumber,bookData) # we must recognize the rating, so we can cast it from a string to a number
         bookData['Rating'] = starRatingScores[starRatingText]
 
         # extract book availability
         availabilities = book.find_all('p',class_="instock availability")
-        assert(len(availabilities)==1) # each book should have only one availability
+        dataValidationAssert(len(availabilities)==1,"ERROR: Book availability missing or duplicated",scrapeNumber,bookData)
         availabilityText = availabilities[0].text
         bookData['Available'] = availabilityText.strip().lower() == "in stock" # this might be too draconian for a real scrape, and should probably have more sophisticated logic to handle different possible cases
 
         # extract book thumbnail
         thumbnails = book.find_all('img',class_="thumbnail",src=True)
-        assert(len(thumbnails)==1) # each book should only have one thumbnail
+        dataValidationAssert(len(thumbnails)==1,"ERROR: Book thumbnail missing or duplicated",scrapeNumber,bookData)
         thumbnail = thumbnails[0]['src']
-        assert(thumbnail.split('.')[-1] in ['jpg','jpeg','png']) # ensure that the thumbnail is an image file. Note: this list of image file types is just off the top of my head, and by no means a complete list, as this is just a coding test. In a real production setting it should be researched more thoroughly.
+        dataValidationAssert(thumbnail.split('.')[-1] in ['jpg','jpeg','png'],"ERROR: Book thumbnail filename is not a known image format",scrapeNumber,bookData) # ensure that the thumbnail is an image file. Note: this list of image file types is just off the top of my head, and by no means a complete list, as this is just a coding test. In a real production setting it should be researched more thoroughly.
         bookData['Thumbnail URL'] = thumbnail
 
         bookDataList.append(bookData)
 
-    # for every book we found before, pull its html page
-    for bookData in bookDataList:
+    ## for every book we found before, pull its html page
+    #for bookData in bookDataList:
 
         # pull up the page for each book
         url = "http://books.toscrape.com/catalogue/"+bookData['Sub-Page Link']
         response = requests.get(url)
-        assert(response.status_code==200) # ensure the page correctly loaded
+        dataValidationAssert(response.status_code==200,f"ERROR: Book sub-page did not load correctly (status code {response.status_code})",scrapeNumber,bookData)
         bookData['Sub-Page Raw HTML'] = response.text
 
-    # for every book's page, scrape data
-    for bookData in bookDataList:
+    ## for every book's page, scrape data
+    #for bookData in bookDataList:
 
         # everything we need is within the article
         soup = bs4.BeautifulSoup(bookData['Sub-Page Raw HTML'],features='html.parser')
         article = soup.find('article')
 
         # extract book description
-        paragraphs = article.findChildren('p',recursive=False) # the only top-level paragraph in the article is our text
-        assert(len(paragraphs)==1) # there should be only one paragraph of text
-        paragraph = paragraphs[0].text
-        bookData['Product Description'] = paragraph
+        if 'Product Description' in [ h2.text for h2 in soup.find_all('h2') ]:
+            paragraphs = article.findChildren('p',recursive=False) # the only top-level paragraph in the article is our text
+            dataValidationAssert(len(paragraphs)==1,"ERROR: Product description missing or duplicated",scrapeNumber,bookData)
+            paragraph = paragraphs[0].text
+            bookData['Product Description'] = paragraph
+        else:
+            bookData['Product Descrption'] = None
 
         # extract book photo
         activeItems = soup.find_all('div',class_='item active')
-        assert(len(activeItems)==1) # each book should only have one active item
+        dataValidationAssert(len(activeItems)==1,"ERROR: Book activte item missing or duplicated",scrapeNumber,bookData)
         photos = activeItems[0].find_all('img',src=True)
-        assert(len(photos)==1) # each book should only have one photo
+        dataValidationAssert(len(photos)==1,"ERROR: Book photo missing or duplicated",scrapeNumber,bookData)
         photo = photos[0]['src']
-        assert(photo.split('.')[-1] in ['jpg','jpeg','png']) # ensure that the photo is an image file. Note: this list of image file types is just off the top of my head, and by no means a complete list, as this is just a coding test. In a real production setting it should be researched more thoroughly.
+        dataValidationAssert(photo.split('.')[-1] in ['jpg','jpeg','png'],"ERROR: Book full image filename is not a known image format",scrapeNumber,bookData) #Note: this list of image file types is just off the top of my head, and by no means a complete list, as this is just a coding test. In a real production setting it should be researched more thoroughly.
         bookData['Full Photo URL'] = photo
 
         # extract the subcategory from the page breadcrumbs
         categories = [ a.text for a in  soup.find_all('a',href=True) if '../category/books/' in a['href'] ]
-        assert(len(categories)==1) # there should only be one sub-category
+        dataValidationAssert(len(categories)==1,"ERROR: Breadcrumbs sub-category missing or duplicated",scrapeNumber,bookData)
         bookData['Product Sub-Category'] = categories[0]
 
         # extract the data table
         tables = article.find_all('table')
-        assert(len(tables)==1) # there should only be one table on this page
+        dataValidationAssert(len(tables)==1,"ERROR: Sub-page data table missing or duplicated",scrapeNumber,bookData)
         table = tables[0]
         tableDict = dict( (row.find('th').text,row.find('td').text) for row in table.find_all('tr') )
         
         # extract simple text fields
         for textField in ['UPC','Product Type']:
-            assert(textField in tableDict) # we need the data to exist in the table
+            dataValidationAssert(textField in tableDict,f"ERROR: text field '{textField}' missing from table",scrapeNumber,bookData)
             bookData[textField] = tableDict[textField]
 
         # extract money fields
         for priceField in ['Price (excl. tax)', 'Price (incl. tax)', 'Tax']:
-            assert(priceField in tableDict) # we need the data to exist in the table
+            dataValidationAssert(priceField in tableDict,f"ERROR: price field '{priceField}' missing from table",scrapeNumber,bookData)
             priceText = tableDict[priceField]
-            assert('£' in priceText) # the price must have a currency symbol. Note: in a production environment we may want to check for other currency symbols, as well.
+            dataValidationAssert('£' in priceText,f"ERROR: price field '{priceField}' missing GBP symbol",scrapeNumber,bookData) #Note: in a production environment we may want to check for other currency symbols, as well.
             price = priceText.split('£')[1]
-            assert(_textIsFloat(price))
+            dataValidationAssert(_textIsFloat(price),f"ERROR: price field '{priceField}' cannot be cast to a float",scrapeNumber,bookData)
             bookData[priceField] = float(price)
 
         # extract availability count
-        assert('Availability' in tableDict) # we need the data to exist in the table
+        dataValidationAssert('Availability' in tableDict,"ERROR: Book availability missing from table",scrapeNumber,bookData)
         if bookData['Available']:
             inventories = re.findall('\(([0-9]*) available\)',tableDict['Availability'])
-            assert(len(inventories)==1)
+            dataValidationAssert(len(inventories)==1,"ERROR: Book inventory quantity (inside availability string) is missing or duplicated",scrapeNumber,bookData)
             bookData['Inventory'] = int(inventories[0])
         else:
             bookData['Inventory'] = 0
     
-# save book data
-if not os.path.exists("./data/"):
-    os.mkdir("./data/")
-with open(f"./data/Kyle_Horn_books_{scrapeNumber}_of_50.csv",'w') as f:
-    writer = csv.writer(f)
+    # save book data
+    if not os.path.exists("./data/"):
+        os.mkdir("./data/")
+    with open(f"./data/Kyle_Horn_books_{scrapeNumber}_of_50.csv",'w') as f:
+        writer = csv.writer(f)
 
-    # table header
-    columnNames = list(sorted(bookData))
-    writer.writerow(columnNames)
+        # table header
+        columnNames = list(sorted(bookData))
+        writer.writerow(columnNames)
 
-    # table body
-    for bookData in bookDataList:
-        row = [ bookData[key] for key in sorted(bookData) ]
-        writer.writerow(row)
+        # table body
+        for bookData in bookDataList:
+            row = [ bookData[key] for key in sorted(bookData) ]
+            writer.writerow(row)
